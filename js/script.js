ae26f10a20bd229cbf2cda69dec3e90b59518c2e
wldnalpsya <wildan11alpasya@gmail.com>
2026-04-09 22:51:47 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// FILTER PROJECT
+function filterProject(category){
+let projects=document.querySelectorAll(".project");
+
+projects.forEach(function(project){
+if(category==="all"){
+project.style.display="block";
+}
+else{
+if(project.classList.contains(category)){
+project.style.display="block";
+}else{
+project.style.display="none";
+}
+}
+});
+}
+
+// FORM VALIDATION
+let form=document.getElementById("contactForm");
+
+if(form){
+form.addEventListener("submit",function(e){
+e.preventDefault();
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let message=document.getElementById("message").value;
+let msg=document.getElementById("formMessage");
+
+if(name==="" || email==="" || message===""){
+msg.innerText="Semua field wajib diisi!";
+msg.style.color="red";
+}
+else{
+msg.innerText="Pesan berhasil dikirim!";
+msg.style.color="lightgreen";
+form.reset();
+}
+});
+}
